a5c3b36e7790674b440aaa500cf2f013cdab4915
Evgeniy Yurchenkov <gear1995V@gmail.com>
2024-04-23 22:31:48 +0500

    update tests

--- src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
@@ -18,15 +18,24 @@ class UserDbStorageTest {
 
     @Test
     public void testFindUserById() {
-        // Подготавливаем данные для теста
-        User newUser = new User("user@email.ru", "vanya123", "1990-01-01", "Ivan Petrov", 1, null);
+        User newUser = new User("user@email.ru", "vanya123", "1990-01-01", "Ivan Petrov", null, null);
+        User secondUser = new User("secondUser@email.ru", "second123", "1990-01-01", "Second name", null, null);
+
         UserDbStorage userStorage = new UserDbStorage(jdbcTemplate);
         userStorage.createUser(newUser);
+        userStorage.createUser(secondUser);
 
-//         вызываем тестируемый метод
         User savedUser = userStorage.findUserById(1);
+        User savedSecondUser = userStorage.findUserById(2);
+        userStorage.addFriend(savedUser.getId(), savedSecondUser.getId());
 
         assertNotNull(savedUser);
-        assertEquals(newUser, savedUser);
+        assertEquals(newUser.getName(), savedUser.getName());
+        assertEquals(newUser.getLogin(), savedUser.getLogin());
+        assertEquals(newUser.getBirthday(), savedUser.getBirthday());
+        assertEquals(newUser.getName(), savedUser.getName());
+
+//        userStorage.addFriend(savedUser.getId(), savedSecondUser.getId());
+        assertEquals(savedUser.getFriends(), savedSecondUser.getFriends());
     }
 }
