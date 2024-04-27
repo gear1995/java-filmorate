0eb78da2847f1b00393aeaa71187bef0fec5710a
Evgeniy Yurchenkov <gear1995V@gmail.com>
2024-04-27 21:33:04 +0500

    add mpa and genres data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -76,3 +76,18 @@ create table IF NOT EXISTS FILM_GENRE
     constraint FILM_GENRE_GENRE_GENRE_ID_FK
         foreign key (GENRE_ID) references GENRE (GENRE_ID)
 );
+
+MERGE INTO GENRE (GENRE_ID, GENRE_NAME)
+    VALUES (1, 'Комедия'),
+           (2, 'Драма'),
+           (3, 'Мультфильм'),
+           (4, 'Триллер'),
+           (5, 'Документальный'),
+           (6, 'Боевик');
+
+MERGE INTO MPA_RATING (MPA_RATING_ID, MPA_RATING_NAME)
+    VALUES (1, 'G'),
+           (2, 'PG '),
+           (3, 'PG-13'),
+           (4, 'R'),
+           (5, 'NC-17');
